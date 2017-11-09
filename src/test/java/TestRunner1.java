c76e1ca2224c495e8d46090cd1a95c92b0674f49
Ferga Dipa Balleka <fergadipab@gmail.com>
2017-11-10 00:34:40 +0700

    initial commit

---
+++ src/test/java/TestRunner1.java
@@ -0,0 +1,9 @@
+import cucumber.api.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(features = "src/test/features/Login.feature")
+public class TestRunner1 {
+
+}
