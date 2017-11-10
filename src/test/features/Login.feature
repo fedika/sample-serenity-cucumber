4f02afd687c71f7d7e365c02a9bfce6febd71497
Ferga Dipa Balleka <fergadipab@gmail.com>
2017-11-11 00:13:32 +0700

    Remove browser width and height Modify scenario Remove chrome option arguments for incognito and user agent

--- src/test/features/Login.feature
+++ src/test/features/Login.feature
@@ -3,8 +3,8 @@ Feature: Login
   Scenario: Customer can login
     Given I open the website
     When I click burger menu
-    When I click My Account menu
-    When I click Login menu
+    And I click My Account menu
+    And I click Login menu
     Then I can see Login page
     When I input email "user@phptravels.com"
     And I input password "demouser"
--- src/test/java/steps/HomeStep.java
+++ src/test/java/steps/HomeStep.java
@@ -12,8 +12,6 @@ public class HomeStep {
     public void openTheWebsite(){
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--window-size=300,700");
-        chromeOptions.addArguments("--incognito");
-        chromeOptions.addArguments("--user-agent=Mozilla/5.0 (Linux; Android 6.0.1; SM-G920V Build/MMB29K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.98 Mobile Safari/537.36");
         ChromeDriver chromeDriver = new ChromeDriver(chromeOptions);
         homePage.setDriver(chromeDriver);
         homePage.getDriver().manage().deleteAllCookies();
